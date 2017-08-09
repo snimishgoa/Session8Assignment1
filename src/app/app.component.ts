a94972b9667d05f6fa38bd75fc5fb0ccbc3499c5
shikerkar <shikerkar@iazi.ch>
2017-08-09 10:07:13 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { UserDataService } from "app/user-data.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [UserDataService]
 })
 export class AppComponent {
-  title = 'app';
+  title = 'My App';
+  hasUsers = false;
+
+  constructor(private _userService: UserDataService) { }
+
+  addUser(name: string): void {
+    if (name) {
+      this._userService.addUser(name);
+      this.hasUsers = true;
+    }
+  }
+
+  getUserList(): string[] {
+    return this._userService.getUserList();
+  }
 }
